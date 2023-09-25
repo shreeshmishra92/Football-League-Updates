e005e5d5865575a5fc7e6ef6d5d95b65ca928cd2
shreeshmishra92 <58977445+shreeshmishra92@users.noreply.github.com>
2023-09-25 19:20:34 +0530

    1 file modified, 4 deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LeagueStandingsComponent } from './dashboard/league-standings/league-standings.component'; 
 import { FixturesComponent } from './fixtures/fixtures.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path:'',  component:DashboardComponent },
-  { path:'fixture/:leagueId/:teamId', component:FixturesComponent  }
+  { path: '', component: DashboardComponent },
+  { path: 'fixture/:leagueId/:teamId', component: FixturesComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
- 
-  exports: [RouterModule]
+
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
