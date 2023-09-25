9f252b417fe6301017d4e2009f9a7f258c9a1d5a
shreeshmishra92 <58977445+shreeshmishra92@users.noreply.github.com>
2023-09-25 17:30:15 +0530

    1 file modified

--- src/app/dashboard/league-standings/league-standings.component.ts
+++ src/app/dashboard/league-standings/league-standings.component.ts
@@ -1,20 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { leagueData } from 'src/app/interface/leagueData';
+
 import { LeagueStandings } from 'src/app/interface/standings';
 
 @Component({
   selector: 'app-league-standings',
   templateUrl: './league-standings.component.html',
-  styleUrls: ['./league-standings.component.css']
+  styleUrls: ['./league-standings.component.css'],
 })
 export class LeagueStandingsComponent implements OnInit {
-@Input() standingsList: LeagueStandings[]=[];
-@Input() leagueId!:number;
-@Input() loading:boolean=false;
-  constructor() { }
+  @Input() standingsList: LeagueStandings[] = [];
+  @Input() leagueId!: number;
+  @Input() loading: boolean = false;
+  constructor() {}
 
   ngOnInit(): void {
-    console.log( this.standingsList);
-    }
-
+    console.log(this.standingsList);
+  }
 }
